c66817970d991807e6f701ca245acfd4c0a60315
Igor Moreira <igorriver2@gmail.com>
2025-03-05 15:22:30 -0300

    Configuração vite

--- Clinica-odontologica/frontend/vite.config.js
+++ Clinica-odontologica/frontend/vite.config.js
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Plugin do React
   build: {
-    outDir: 'dist',
-    //rollupOptions: {
-      //input: {
-        //main: './index.html', // Caminho para o index.html
-      //},
- //   },
+    outDir: 'dist', // Pasta de saída para os arquivos construídos
+    rollupOptions: {
+      input: {
+        main: './index.html', // Especifica o arquivo de entrada HTML
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      // Configura aliases para importações (opcional)
+      '@': '/src', // Exemplo: usar '@/' para referenciar a pasta 'src'
+    },
+  },
+  server: {
+    port: 3000, // Porta do servidor de desenvolvimento
+    open: true, // Abre o navegador automaticamente ao iniciar o servidor
   },
 });
